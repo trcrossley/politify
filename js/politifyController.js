114d74ae6ff9daa7397eabb9e352fc33f6b9c5b5
gareth4192 <gareth-123@live.com>
2016-02-10 10:36:09 +0000

    conflicts removed

--- js/politifyController.js
+++ js/politifyController.js
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
+
 politify.controller('PolitifyController', ['MpSearch', function (MpSearch) {
-=======
-politify.controller('PolitifyController', ['ConstituencySearch', function (ConsituencySearch) {
->>>>>>> 239cf05264e91001d8e2aaf2ec9f38b0bfbb5008
+
 
   var self = this;
   self.postcode = '';
